b3d7dee6113b874aea5310b558413a63acb3c0f1
HungDTTS2208004 <117142252+HungDTTS2208004@users.noreply.github.com>
2022-11-19 08:47:24 +0700

    A lot of Changes

--- System.js
+++ System.js
@@ -1,15 +1,76 @@
-var app = angular.module("myApp", []);
-app.controller("navController",function($scope){
-    $scope.navShow = true;
-    $scope.mobileProductDropdown = true;
-    $scope.mobileCollectionDropdown = true;
-    $scope.Login = false;
+
+var setting = {
+    "Home":{
+            "mobileCollDropdown":false,
+            "mobileProductDropdown":false
+        },
+    "Products":{
+            "brandFilter":false,
+            "cateFilter":false,
+            "colorFilter":false
+    }
+}
+$(document).ready(function(){
+    $('.brandFilterContent').hide();
+    $('.cateFilterContent').hide();
+    $('.colorFilterContent').hide();
+    $('.mobileProductDropdown').hide();
 })
-app.controller("productsController",function ($scope) {
-    $scope.brandFilterSH = true;
-    $scope.cateFilterSH = true;
-    $scope.colorFilterSH = true;
-})
-app.controller("productInfoController",function ($scope) {
-    $scope.descriptionShow = false;
-})
+localStorage.setting;
+// Home Setting
+$('.collDropDownBtn').click(function(){ 
+    setting.Home.mobileCollDropdown = !setting.Home.mobileCollDropdown;
+    $('.mobileCollDropdownNav').toggle(100);
+    if (setting.Home.mobileCollDropdown){
+        $('.collDropDownBtn').html("-");
+    } else {
+        $('.collDropDownBtn').html("+");
+    }
+});
+
+$('.mobileProductDropdownBtn').click(function(){
+    setting.Home.mobileProductDropdown = !setting.Home.mobileProductDropdown;
+    if (setting.Home.mobileProductDropdown){
+        $('.mobileProductDropdownBtn').html("-");
+    } else {
+        $('.mobileProductDropdownBtn').html("+");
+    }
+    $('.mobileProductDropdown').toggle(100);
+    
+});
+
+
+
+// Products Setting
+$('.brandFilterBtn').click(function(){
+    setting.Products.brandFilter = !setting.Products.brandFilter;
+    if (setting.Products.brandFilter){
+        $('.showHideBrandIcon').html("-");
+    } else {
+        $('.showHideBrandIcon').html("+");
+    }
+    $('.brandFilterContent').toggle(100);
+    
+});
+
+$('.cateFilterBtn').click(function(){
+    setting.Products.cateFilter = !setting.Products.cateFilter;
+    if (setting.Products.cateFilter){
+        $('.showHideCateIcon').html("-");
+    } else {
+        $('.showHideCateIcon').html("+");
+    }
+    $('.cateFilterContent').toggle(100);
+    
+});
+
+$('.colorFilterBtn').click(function(){
+    setting.Products.colorFilter = !setting.Products.colorFilter;
+    if (setting.Products.colorFilter){
+        $('.showHideColorIcon').html("-");
+    } else {
+        $('.showHideColorIcon').html("+");
+    }
+    $('.colorFilterContent').toggle(100);
+    
+});
